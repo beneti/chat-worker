c5eeade728f5178e24e11433e65b2e0b047887ec
beneti <beneti@beneti.com.br>
2013-04-05 21:35:15 -0300

    Simply

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
+var amqp = require('amqp');
 
-/**
- * Module dependencies.
- */
+function pub_and_sub() {
+  var exchange = conn.exchange(''); // get the default exchange
+  var queue = conn.queue('queue1', {}, function() { // create a queue
+    queue.subscribe(function(msg) { // subscribe to that queue
+      console.log(msg.body); // print new messages to the console
+    });
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    // publish a message
+    exchange.publish(queue.name, {body: 'Hello CloudAMQP!'});
+  });
+}
 
-var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+var url = process.env.CLOUDAMQP_URL || "amqp://localhost"; // default to localhost
+var conn = amqp.createConnection({url: url}); // create the connection
+conn.on('ready', pub_and_sub); // when connected, call "pub_and_sub"
